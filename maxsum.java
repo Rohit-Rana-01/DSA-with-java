573373b8a1a048c9f39e7d313730baf41c6bda0d
Rohit Rana <rohitrana3122000@gmail.com>
2026-01-06 21:40:01 +0530

    modifying the kadanes algo for all neagtive vlaues in array

--- maxsum.java
+++ maxsum.java
@@ -10,8 +10,32 @@ public class maxsum {
         }
         return maxsum ;
     }
+
+    // modifying it for all negative values 
+    public static  int maxSubArray(int[] nums) {
+        int csum = 0;
+        int largest = Integer.MIN_VALUE ;
+        int maxsum = Integer.MIN_VALUE ; 
+        boolean check = false;
+
+        for(int i =0 ; i<nums.length ;i ++){
+            if(nums[i]>0){
+                check = true ;
+            }
+          largest = Math.max(largest , nums[i]);
+          csum = nums[i] + csum;
+          if(csum < 0) csum = 0 ;
+          if(csum > maxsum) maxsum = csum ;
+        }
+
+
+        if(check == true){
+            return maxsum ;
+        }
+        return largest ;
+    }
     public static void main(String[] args){
-        int array[] = {-2, 1,-3,4,-1,2,1,-5,4};
-        System.out.println(kadanes(array));
+        int array[] = {-2};
+        System.out.println(maxSubArray(array));
         }
  }
