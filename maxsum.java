842dc4e4d34b611977eab93e26fb1ae79ac16add
Rohit Rana <rohitrana3122000@gmail.com>
2026-01-06 21:50:09 +0530

    more modification in kadanes alog for more fine code

--- maxsum.java
+++ maxsum.java
@@ -34,8 +34,19 @@ public class maxsum {
         }
         return largest ;
     }
+
+    // final kadanes algo more appropriate  ;
+    public static int finalmaxsubArray(int []nums){
+        int csum = nums[0] ;
+        int maxsum = nums[0] ;
+        for(int i=0 ; i<nums.length ; i++){
+            csum = Math.max(csum +nums[i] , nums[i]);
+            maxsum = Math.max( csum ,maxsum );
+        }
+        return maxsum ;
+    }
     public static void main(String[] args){
-        int array[] = {-2};
-        System.out.println(maxSubArray(array));
+        int array[] = {-2, -1,-3,4,-1,2,1,-5,4};
+        System.out.println(finalmaxsubArray(array));
         }
  }
