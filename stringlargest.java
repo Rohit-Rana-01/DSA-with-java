250d9a097cc1870ca5372b4619bbecefb5aba85c
Rohit Rana <rohitrana3122000@gmail.com>
2026-01-09 22:17:08 +0530

    using StringBuilder class to build the string and reducing the complecities as Strings are imutable and sb helps to create it mutable

--- stringlargest.java
+++ stringlargest.java
@@ -2,6 +2,7 @@ public class stringlargest {
     public static void main(String[] args){
         String fruits[] = {"apple","banana","mango"};
         String largest = fruits[0];
+// compareTo uses lexicographicaly comparisions .. ..
         for(int i =1 ; i<fruits.length;i++){
             if(largest.compareTo(fruits[i]) < 0){
                 largest= fruits[i];
